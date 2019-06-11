3a0bdbb00c27c042cc03ed4e0bc40fba7a8bb123
alejogs4 <alejogs4@gmail.com>
2019-06-10 21:07:05 -0500

    Agrega boilerplate inicial

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,34 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
 
 module.exports = {
-  /* Your site config here */
+  plugins: [
+    'gatsby-plugin-offline',
+    'gatsby-plugin-react-helmet',
+    /**
+    * TODO: Icons support
+    */
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'EDteam',
+        short_name: 'EDteam',
+        start_url: '/',
+        background_color: '#007BDF',
+        theme_color: '#FAFDFF',
+        display: 'standalone',
+      },
+    },
+    // Sass support
+    {
+      resolve: 'gatsby-plugin-sass',
+      options: {
+        implementation: require('sass'),
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        trackingId: "YOUR_GOOGLE_ANALYTICS_TRACKING_ID",
+      },
+    },
+  ],
 }
