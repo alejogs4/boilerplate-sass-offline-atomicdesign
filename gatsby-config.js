60c02f72b9649c4e5e1e324c8938080ab430cf09
alejogs4 <alejogs4@gmail.com>
2019-06-13 15:26:53 -0500

    Agrega carpetas de componentes y de sass

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,10 @@
-
 module.exports = {
   plugins: [
-    'gatsby-plugin-offline',
     'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sass',
     /**
-    * TODO: Icons support
-    */
+     * Modifica estas opciones para customizar la pwa a tus necesidades
+     */
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
@@ -15,20 +14,30 @@ module.exports = {
         background_color: '#007BDF',
         theme_color: '#FAFDFF',
         display: 'standalone',
+        icons: [
+          {
+            "src": "/favicons/edteam-512.png",
+            "sizes": "512x512",
+            "type": "image/png"
+          },
+          {
+            "src": "/favicons/edteam-96.png",
+            "sizes": "96x96",
+            "type": "image/png"
+          },
+          {
+            "src": "/favicons/edteam-128.png",
+            "sizes": "128x128",
+            "type": "image/png"
+          },
+          {
+            "src": "/favicons/edteam-256.png",
+            "sizes": "256x256",
+            "type": "image/png"
+          }
+        ]
       },
     },
-    // Sass support
-    {
-      resolve: 'gatsby-plugin-sass',
-      options: {
-        implementation: require('sass'),
-      },
-    },
-    {
-      resolve: 'gatsby-plugin-google-analytics',
-      options: {
-        trackingId: "YOUR_GOOGLE_ANALYTICS_TRACKING_ID",
-      },
-    },
+    'gatsby-plugin-offline',
   ],
 }
